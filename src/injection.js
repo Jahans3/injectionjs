379237d8366063774db09bd95ae3cc0601a2c5d1
Joshua Jahans <joshua.jahans@thehutgroup.com>
2015-11-28 01:24:45 +0000

    thing

--- src/injection.js
+++ src/injection.js
@@ -1,3 +1,18 @@
 /**
  * Created by jahansj on 27/11/2015.
  */
+
+var main = document.getElementsByClassName('container')[0];
+
+main.style.height = '150px';
+main.style.width = '150px';
+main.style.backgroundColor = 'red';
+
+var interval = 0;
+
+interval = 50 ? setInterval(function(){
+    main.style.height = (interval * 3).toString() + 'px';
+    main.style.height = (interval * 3).toString() + 'px';
+    interval++;
+},50) : null;
+
