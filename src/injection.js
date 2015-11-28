931ea1467ef5e421aed746e3ca31c76a49ea1c49
Joshua Jahans <joshua.jahans@thehutgroup.com>
2015-11-28 01:46:54 +0000

    vodka

--- src/injection.js
+++ src/injection.js
@@ -4,15 +4,12 @@
 
 var main = document.getElementsByClassName('container')[0];
 
-main.style.height = '150px';
-main.style.width = '150px';
+main.style.height = '0px';
+main.style.width = '0px';
 main.style.backgroundColor = 'red';
 
-var interval = 0;
 
-interval = 50 ? setInterval(function(){
-    main.style.height = (interval * 3).toString() + 'px';
-    main.style.height = (interval * 3).toString() + 'px';
-    interval++;
-},50) : null;
+
+
+
 
